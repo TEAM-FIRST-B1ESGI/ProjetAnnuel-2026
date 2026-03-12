25379128b87bf1bac2855636798fa03a68b30c51
Virginie Ollivrin <vo-web@protonmail.com>
2026-03-12 14:28:26 +0100

    HTML et CSS bloc revenu

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <!--<link rel="stylesheet" href="assets/css/style.css">-->
+    <link rel="stylesheet" href="assets/css/style.css?php echo time(); ?>">
     <title>Titre de la page</title>
 </head>
 <body>
     <!--------Bloc nav---------->
     <div class="header">
-        <img class="logo" src="assets/img/logo.png"    
+        <div>
+            <img class="logo" src="assets/img/logo.png">
+        </div>    
         <div class="item-revenu">
-            <p>MON REVENU (€)</p>
-            <button>Modifier</button>
+            <strong>MON REVENU (€)</strong>
+            <div class="montant">
+                <div class="affichage-montant">
+                    2005,00
+                </div>
+                <button class="btn-modifier">Modifier</button>
+            </div>
         </div>
     </div>
 
 
     <!--------Bloc point dépense---------->
+    <section>
+        <div class="bloc-repartion-depenses"></div>
+        <div class="bloc-alertes"></div>
+    </section>
 
     <!--------Bloc fonctionnalité épargne restante---------->
 
