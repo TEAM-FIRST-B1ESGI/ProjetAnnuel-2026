12e179d1c4658e2a21c2fe46a9f6787b33722d24
Virginie Ollivrin <vo-web@protonmail.com>
2026-03-12 10:58:28 +0100

    Commit initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Mon site PHP
+
+Site web développé en PHP sans framework.
+
+## Technologies
+- PHP
+- HTML
+- CSS
+- JavaScript
+
+## Installation
+
+1. Cloner le dépôt
+2. Placer le projet dans le dossier serveur (htdocs ou www)
+3. Lancer Apache
+4. Ouvrir http://localhost/projetA2026
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Titre de la page</title>
+</head>
+<body>
+    <!--------Bloc nav---------->
+    <img src="assets/img/logo.png"
+    <h1>Titre principal</h1>
+    
+    <div class="item-revenu"><p>MON REVENU (€)</p>
+        <div>btn-modifier</div>
+    </div>
+    <!--------Bloc point dépense---------->
+
+    <!--------Bloc fonctionnalité épargne restante---------->
+
+    <!--------Bloc intéractif décla dépenses---------->
+
+    <!--------Bloc historique---------->
+</body>
+</html>
